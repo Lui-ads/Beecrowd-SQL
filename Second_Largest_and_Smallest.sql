7e7d9a41d9766dfb5e7f9695105e5214d0d0ae93
MadridMiranda <madrid2005miranda@gmail.com>
2026-04-24 14:52:23 -0300

    Create Second_Largest_and_Smallest.sql

---
+++ Second_Largest_and_Smallest.sql
@@ -0,0 +1,13 @@
+-- 3483 - Second Largest and Smallest
+
+(SELECT city_name, population
+FROM cities
+ORDER BY population DESC
+LIMIT 1 OFFSET 1)
+
+UNION ALL
+
+(SELECT city_name, population
+FROM cities
+ORDER BY population ASC
+LIMIT 1 OFFSET 1);
